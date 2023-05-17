2618c8b0bd567c246bf93612c520955fdbf13d9b
McNaBry <mcnabry123@gmail.com>
2023-05-17 18:14:06 +0800

    Abstracted the tech stack into its own component, shifted images to public folder and cleaned up code in About.tsx

--- grindtogether/src/components/About.tsx
+++ grindtogether/src/components/About.tsx
@@ -1,53 +1,98 @@
+import React from "react";
+
+// Tech Stack Component
+function TechLogo({ imgLink, imgAlt, imgID, webLink }) {
+  return( 
+  <a
+    target="_blank"
+    href={webLink}
+    id={imgID}
+  >
+    <img src={imgLink} alt={imgAlt} className="logo"/>
+  </a>)
+}
+
+function TechLogoBar() {
+  const imgData = [
+    ["images/html5-logo.png", "HTML 5", "html-logo", 
+      "https://developer.mozilla.org/en-US/docs/Web/HTML"],
+    ["images/react-logo.png", "React JS", "react-logo", 
+      "https://reactjs.org"],
+    ["images/bootstrap-logo.png", "BootStrap 5", "bootstrap-logo",
+      "https://getbootstrap.com/"],
+    ["images/nodejs-logo.png", "Node JS", "nodejs-logo",
+      "https://nodejs.org/en/about"],
+    ["images/firebase-logo.png", "Firebase", "firebase-logo",
+      "https://firebase.google.com/"],
+    ["images/netlify-logo.png", "Netlify", "netlify-logo",
+      "https://www.netlify.com/"]]
+
+  const logos = imgData.map(data => {
+    return (
+      <TechLogo key={data[2]}
+        imgLink={data[0]} imgAlt={data[1]}
+        imgID={data[2]} webLink={data[3]} /> 
+    )
+  })
+
+  return <div className="tech-container">{logos}</div>
+}
+
+function TechStack() {
+  return (
+    <div className="tech-stacks">
+      <p className="header"> This web app is powered by </p>
+      <TechLogoBar/>
+    </div>
+  )
+}
+
 function About() {
   return (
     <>
       <h1 className="about-header"> About GrindTogether </h1>
       <div className="motivation">
-        <div className="motivation-section">
-          <h2 className="header"> Our motivation </h2>
-          <div className="motivation-text">
-            <p>
-              {" "}
-              Have you ever felt so lost in a certain module with very few
-              friends to ask for help for this module? Well, many of us have
-              certainly felt that way before and we all know how tough and
-              demanding certain modules are. The thought of having to tackle a
-              module alone, especially if it has a reputation of being
-              difficult, is truly very frightening. We might also feel a bit
-              lonely studying by ourselves.{" "}
-            </p>
+        <h2 className="header"> Our motivation </h2>
+        <img src="images/motivation-pic.png" alt="motivation picture" id="motivation-pic"></img>
+        <div className="motivation-text">
+          <p>
+            {" "}
+            Have you ever felt so lost in a certain module with very few
+            friends to ask for help for this module? Well, many of us have
+            certainly felt that way before and we all know how tough and
+            demanding certain modules are. The thought of having to tackle a
+            module alone, especially if it has a reputation of being
+            difficult, is truly very frightening. We might also feel a bit
+            lonely studying by ourselves.{" "}
+          </p>
 
-            <p>
-              It would be comforting and beneficial if someone coping better
-              with the module could offer their help and moral support, be it a
-              senior with past experience or a fellow mod-taker. At least by
-              finding people doing the same module we can learn from one another
-              and find solace in each other. In university, where people come
-              and go and everyone is busy with academics, it is hard for many of
-              us to connect with others and moreover, find people to study with.
-              Wouldn’t it be great if there existed a way (without the initial
-              “approaching others in real life”) to see if others in NUS are
-              willing to offer their help, or if you’re feeling kind, find those
-              who need it?{" "}
-            </p>
+          <p>
+            It would be comforting and beneficial if someone coping better
+            with the module could offer their help and moral support, be it a
+            senior with past experience or a fellow mod-taker. At least by
+            finding people doing the same module we can learn from one another
+            and find solace in each other. In university, where people come
+            and go and everyone is busy with academics, it is hard for many of
+            us to connect with others and moreover, find people to study with.
+            Wouldn’t it be great if there existed a way (without the initial
+            “approaching others in real life”) to see if others in NUS are
+            willing to offer their help, or if you’re feeling kind, find those
+            who need it?{" "}
+          </p>
 
-            <p>
-              {" "}
-              Our project wants to make it easier for everyone by providing a
-              multifunctional platform, GrindTogether, which can be used to find
-              groups of friends with similar modules to study together or even
-              find study date partners (you never know what can happen...
-              &#128064 &#128527).
-            </p>
-          </div>
-        </div>
-        <div className="motivation-pic">
-          <img src="images/motivation-pic.png" alt="motivation picture"></img>
+          <p>
+            {" "}
+            Our project wants to make it easier for everyone by providing a
+            multifunctional platform, GrindTogether, which can be used to find
+            groups of friends with similar modules to study together or even
+            find study date partners (you never know what can happen...
+            &#128064 &#128527).
+          </p>
         </div>
       </div>
 
       <div className="mission">
-        <h2 className="mission header"> Our mission </h2>
+        <h2 className="header"> Our mission </h2>
         <p>
           {" "}
           We want to help fellow students in NUS have a better study experience
@@ -83,64 +128,7 @@ function About() {
         </div>
       </div>
 
-      <div className="tech-stacks">
-        <p className="header"> This web app is powered by </p>
-        <div className="tech-container">
-          <a
-            target="_blank"
-            href="https://developer.mozilla.org/en-US/docs/Web/HTML"
-            id="html-logo"
-            className="logo"
-          >
-            <img src="images/html5-logo.png" alt="HTML 5" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://reactjs.org"
-            id="react-logo"
-            className="logo"
-          >
-            <img src="images/react-logo.png" alt="React JS" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://getbootstrap.com/"
-            id="bootstrap-logo"
-            className="logo"
-          >
-            <img src="images/bootstrap-logo.png" alt="BootStrap 5" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://nodejs.org/en/about"
-            id="nodejs-logo"
-            className="logo"
-          >
-            <img src="images/nodejs-logo.png" alt="Node JS" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://firebase.google.com/"
-            id="firebase-logo"
-            className="logo"
-          >
-            <img src="images/firebase-logo.png" alt="Firebase" />
-          </a>
-
-          <a
-            target="_blank"
-            href="https://www.netlify.com/"
-            id="netlify-logo"
-            className="logo"
-          >
-            <img src="images/netlify-logo.png" alt="Netlify" />
-          </a>
-        </div>
-      </div>
+      <TechStack />
     </>
   );
 }
